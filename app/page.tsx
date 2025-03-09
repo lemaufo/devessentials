377358ed2732376a57ac7df5082d6cf811013ccb
lemaufo <leibertrejo8@gmail.com>
2025-03-09 16:44:19 -0600

    Add 3d-card component to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import {AnimatedPinDemo} from "../components/card-item";
+
 function WelcomePage() {
   return (
-    <div>Bienvenido!!!</div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+      <AnimatedPinDemo />
+    </div>
   )
 }
 export default WelcomePage
