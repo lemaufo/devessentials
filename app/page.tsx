b04caa8d635746b31397ae8968712000b86b0813
leiberjrl <leibertrejo8@gmail.com>
2025-03-11 10:28:57 -0600

    Add glowing cards to Welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import {CoverDemo} from "../components/cover-text"
+import {GlowingEffectDemo} from "../components/glowing-effect"
 
 export default function WelcomePage() {
   return (
-    <main className="">
+    <main className="container mx-auto p-8">
       <div className="">
-          <CoverDemo
-            
+          <CoverDemo   
           />
+      </div>
+      <div className="mt-16 mb-8">
+          <GlowingEffectDemo />
       </div>
     </main>
   )
